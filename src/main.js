15b34565bd5c9d1a4d4525bbb4d2b2c005d8fc26
Rick Stevens <rick.stevens@greenhousegroup.com>
2020-05-11 09:54:07 +0200

    Try Axios

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
-import 'isomorphic-fetch';
+import axios from 'axios';
 
 export default async () => {
-  const response = await fetch('https://jsonplaceholder.typicode.com/todos/1');
-  const result = await response.json();
-  return result;
+  const response = await axios('https://jsonplaceholder.typicode.com/todos/1');
+  return response;
 };
